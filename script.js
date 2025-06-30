68c331686d5d67dee0c268c4a6333b0316d2b7c9
glitch-master332 <gymsharkinfo1@gmail.com>
2025-06-30 12:04:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function login() {
+    window.location.href = "dashboard.html";
+}
+function logout() {
+    window.location.href = "login.html";
+}
+function toggleDarkMode() {
+    document.body.classList.toggle('dark');
+}
