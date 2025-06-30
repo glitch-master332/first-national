68e00bde263ff1c534884c6442e8921fb5ae4913
glitch-master332 <gymsharkinfo1@gmail.com>
2025-06-30 14:56:44 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    window.location.href = "dashboard.html";
-}
-function logout() {
-    window.location.href = "login.html";
-}
-function toggleDarkMode() {
-    document.body.classList.toggle('dark');
-}
